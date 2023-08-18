a0b0b34e0e08343ae9b7897e15dbc250c0e7b50b
Natalia Diaz <nataliadiazc@hotmail.com>
2023-08-18 16:10:04 -0500

    Actividad 2 y explicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,9 @@ import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HomeComponent } from './components/home/home.component';
 import { Nivel1Component } from './components/nivel1/nivel1.component';
-import { Nivel2Component } from './components/nivel2/nivel2.component';
 import { Nivel2Module } from './components/nivel2/nivel2.module';
 import { FormsModule } from '@angular/forms';
 import { ExplicacionComponent } from './components/explicacion/explicacion.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
